c94b3de07e57cbaf0d4d5386d9f212b0bb6a17d8
dataholics-selfience <48730751+dataholics-selfience@users.noreply.github.com>
2025-09-09 09:59:50 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
